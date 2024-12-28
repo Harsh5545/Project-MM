766565c116f6348f9e8296fc6c10b183d6c083f7
Harsh <harshadkajale333@gmail.com>
2024-12-28 12:40:19 +0530

    add

--- src/app/(home)/sign-in/page.jsx
+++ src/app/(home)/sign-in/page.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full flex mt-20 justify-center'>
+    <div className='w-full flex h-screen items-center justify-center'>
       <div className='flex flex-col w-[400px]'>
-        <h1 className='text-4xl w-full text-center font-bold mb-10'>Sign In</h1>
+  
         <div className='flex flex-col gap-4 mx-2 py-2'>
           <LoginForm />
         </div>
