7b6ef33c283a75c024472988f1cc5299d9df4f43
Harsh <harshadkajale333@gmail.com>
2024-12-28 19:48:27 +0530

    add

--- src/components/Navbar/HeaderDefault.jsx
+++ src/components/Navbar/HeaderDefault.jsx
@@ -35,7 +35,7 @@ function HeaderDefault() {
 
   return (
     <div className="flex z-[999] absolute w-full pb-5 justify-center items-center">
-      <div className="flex-col bg-black text-white bg-opacity-30 md:flex-row flex justify-between px-2 md:px-10 rounded-md w-[90%] items-center backdrop-filter backdrop-blur-md py-1 mt-4">
+      <div className="flex-col bg-black text-white bg-opacity-50 md:flex-row flex justify-between px-2 md:px-10 rounded-md w-[90%] items-center backdrop-filter backdrop-blur-md py-1 mt-4">
         <div className="flex justify-between items-center w-full md:w-0">
           <span className="text-xl">
             <Image
@@ -59,7 +59,7 @@ function HeaderDefault() {
                   className="line line-top-bottom"
                   d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
                 ></path>
-                <path className="line" d="M7 16 27 16"></path>
+                <path className="line text-white" d="M7 16 27 16"></path>
               </svg>
             </label>
           </div>
