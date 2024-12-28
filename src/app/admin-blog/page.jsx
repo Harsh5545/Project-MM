fc5cc5b4f3dfb638eee6cb39d5041389c2652ca3
Harsh <harshadkajale333@gmail.com>
2024-12-28 12:53:21 +0530

    add

--- src/app/admin-blog/page.jsx
+++ src/app/admin-blog/page.jsx
@@ -6,8 +6,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='h-screen flex justify-center items-center'> 
-      <SignInButton />
-      <LoginGithub/>
+      <AddBlog />
     </div>
   )
 }
