72e9157123b1b10c7ef0ec63447d1fabd5857d36
Bhavish <Bhavishm009@gmail.com>
2025-01-08 19:10:37 +0530

    chnage

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,11 @@
 export default function robots() {
     return {
-      rules: {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/admin/','/user/'
-      },
-      sitemap: 'https://acme.com/sitemap.xml',
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/admin/',
+            disallow: '/user/',
+        },
+        sitemap: 'https://acme.com/sitemap.xml',
     }
-  }
+}
