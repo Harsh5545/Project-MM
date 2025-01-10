4a90c3827d82d11f115d503bb97b00a7c50e3fe7
Harsh <harshadkajale333@gmail.com>
2025-01-10 23:24:26 +0530

    navbar

--- src/components/Navbar/links/AllLinks.jsx
+++ src/components/Navbar/links/AllLinks.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { useMediaQuery } from "react-responsive";
-import { ChevronDown } from 'lucide-react';
+import React, { useState, useEffect, useRef } from "react";
+import Navlink from "../navlink/Navlink";
+import { useRouter } from "next/navigation";
 
-const AllLinks = ({ onClose, onNavigate }) => {
-  const [openDropdown, setOpenDropdown] = useState(null);
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const isTablet = useMediaQuery({ query: "(min-width: 768px) and (max-width: 1023px)" });
+const AllLinks = ({onClose}) => {
+  const router = useRouter();
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const [hoveredSubLink, setHoveredSubLink] = useState(null);
+  const [isMobile, setIsMobile] = useState(false);
+  const closeTimeoutRef = useRef(null);
 
   const links = [
     { title: "Home", path: "/" },
@@ -20,124 +21,181 @@ const AllLinks = ({ onClose, onNavigate }) => {
           title: "Personality Transformation",
           path: "/servicesality-transformation",
           subLinks: [
-            { title: "For Men", path: "/servicesality-transformation/Men" },
-            { title: "For Women", path: "/servicesality-transformation/Women" },
+            {
+              title: "Personality Transformation for Men",
+              path: "/servicesality-transformation/Men",
+            },
+            {
+              title: "Personality Transformation for Women",
+              path: "/servicesality-transformation/Women",
+            },
           ],
         },
         {
           title: "Corporate Grooming",
           path: "/servicesate-grooming",
           subLinks: [
-            { title: "Business Etiquette", path: "/servicese-business-etiquette-&-corporate-grooming" },
-            { title: "In-house Training", path: "/servicesse-Corporate-Training" },
+            {
+              title: "Bespoke Business Etiquette & Corporate Grooming",
+              path: "/servicese-business-etiquette-&-corporate-grooming",
+            },
+            {
+              title: "In-house Corporate Training",
+              path: "/servicesse-Corporate-Training",
+            },
           ],
         },
-        { title: "Children's Etiquette", path: "/services/3" },
+        { title: "Children's Etiquette", path: "/services/3"},
         { title: "Young Adult Etiquette", path: "/servicesadult-etiquette" },
         {
           title: "Latest Workshop",
           path: "/services-workshop",
           subLinks: [
-            { title: "Ladies Grooming", path: "/services-workshop/ladies-grooming" },
-            { title: "Young Adult Grooming", path: "/services-workshop/young-adult-grooming" },
-            { title: "Young Adult Training", path: "/services-workshop/young-adult" },
-            { title: "Dining Etiquette", path: "/services-workshop/dining-etiquette" },
+            {
+              title: "Ladies Grooming & Social Etiquette Programme",
+              path: "/services-workshop/ladies-grooming",
+            },
+            {
+              title: "Young Adult-Grooming & Etiquette",
+              path: "/services-workshop/young-adult-grooming",
+            },
+            {
+              title: "Young Adult Training",
+              path: "/services-workshop/young-adult",
+            },
+            {
+              title: "Dining Etiquette Workshop",
+              path: "/services-workshop/dining-etiquette",
+            },
           ],
         },
+        // { title: "Train the Trainer", path: "/service/train-the-trainer" },
       ],
     },
     { title: "Blog", path: "/blogs" },
   ];
 
-  const handleDropdownToggle = (index) => {
-    setOpenDropdown(openDropdown === index ? null : index);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // Check on initial render
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleMouseLeave = () => {
+    closeTimeoutRef.current = setTimeout(() => {
+      setOpenDropdown(false);
+      setHoveredSubLink(null);
+    }, 300); // Delay of 300ms to avoid flickering
   };
 
-  const handleLinkClick = (path) => {
-    onNavigate(path);
-    if (onClose) onClose();
+  const handleMouseEnter = () => {
+    if (closeTimeoutRef.current) {
+      clearTimeout(closeTimeoutRef.current);
+    }
+    setOpenDropdown(true);
   };
 
-  const dropdownVariants = {
-    hidden: { 
-      opacity: 0,
-      y: -5,
-      transition: {
-        duration: 0.2,
-      }
-    },
-    visible: { 
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.2,
-      }
-    },
+  const handleSubLinkMouseEnter = (title) => {
+    if (closeTimeoutRef.current) {
+      clearTimeout(closeTimeoutRef.current);
+    }
+    setHoveredSubLink(title);
+  };
+
+  const handleSubLinkMouseLeave = () => {
+    closeTimeoutRef.current = setTimeout(() => {
+      setHoveredSubLink(null);
+    }, 300); // Delay to prevent the flicker
   };
 
   return (
-    <nav className={`${isMobile || isTablet ? 'space-y-4' : 'space-x-6'} flex ${isMobile || isTablet ? 'flex-col' : 'flex-row'}`}>
+    <div className={`flex ${isMobile ? "flex-col space-y-4" : "flex-row space-x-6"} justify-center items-center`}>
       {links.map((link, i) => (
-        <div key={i} className="relative group">
+        <div key={i} className="relative">
           {!link.subLinks ? (
-            <button
-              onClick={() => handleLinkClick(link.path)}
-              className="font-medium text-base cursor-pointer hover:text-primary transition-colors duration-200"
-            >
-              {link.title}
-            </button>
+          <div onClick={()=>{
+            if(isMobile) {
+              onClose()
+            }
+          }}>
+            <Navlink item={link}/>
+          </div>
           ) : (
-            <div>
+            <div
+              className="inline-block"
+              onMouseEnter={!isMobile ? handleMouseEnter : undefined}
+              onMouseLeave={!isMobile ? handleMouseLeave : undefined}
+            >
               <button
-                className="font-medium text-base cursor-pointer flex items-center group hover:text-primary transition-colors duration-200"
-                onClick={() => handleDropdownToggle(i)}
-                aria-expanded={openDropdown === i}
+                className="font-medium text-base cursor-pointer flex items-center"
+                onClick={() => {
+                  if (isMobile) {
+                    router.push(link.path); // Directly navigate on mobile
+                    onClose()
+                  } else {
+                    setOpenDropdown(!openDropdown);
+                  }
+                }}
               >
                 {link.title}
-                <ChevronDown className={`ml-1 w-4 h-4 transition-transform duration-200 ${openDropdown === i ? 'rotate-180' : ''}`} />
+                {!isMobile && ( // Show dropdown arrow only for desktop view
+                  <svg
+                    className="ml-2 w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                  </svg>
+                )}
               </button>
 
-              <AnimatePresence>
-                {((isMobile || isTablet) && openDropdown === i) || (!isMobile && !isTablet && (openDropdown === i || (!isMobile && !isTablet))) ? (
-                  <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    variants={dropdownVariants}
-                    className={`${isMobile || isTablet ? 'mt-2' : 'absolute mt-2 left-0'} bg-white dark:bg-gray-800 rounded-lg shadow-lg z-10 ${isMobile || isTablet ? 'w-full' : 'w-56'}`}
-                  >
-                    {link.subLinks.map((subLink, j) => (
-                      <div key={j} className="relative group">
-                        <button
-                          className="block px-4 py-2 text-sm font-semibold text-left w-full hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors duration-200"
-                          onClick={() => handleLinkClick(subLink.path)}
-                        >
-                          {subLink.title}
-                        </button>
+              {!isMobile && openDropdown && (
+                <div className="absolute mt-2 bg-opacity-60 bg-black rounded-lg shadow-lg z-10">
+                  {link.subLinks.map((subLink, j) => (
+                    <div
+                      key={j}
+                      className="relative group"
+                      onMouseEnter={() => handleSubLinkMouseEnter(subLink.title)}
+                      onMouseLeave={handleSubLinkMouseLeave}
+                    >
+                      <button
+                        className="block px-4 py-2 text-sm font-semibold text-left w-full text-nowrap"
+                        onClick={() => router.push(subLink.path)}
+                      >
+                        {subLink.title}
+                      </button>
 
-                        {subLink.subLinks && (
-                          <div className={`${isMobile || isTablet ? 'ml-4' : 'absolute left-full top-0'} bg-white dark:bg-gray-800 rounded-lg shadow-lg ${isMobile || isTablet ? 'w-full' : 'w-56'}`}>
+                      {hoveredSubLink === subLink.title && subLink.subLinks && (
+                        <div
+                          className="absolute left-60 top-0 w-60 bg-opacity-60 bg-black shadow-lg rounded-lg"
+                          onMouseEnter={() => handleSubLinkMouseEnter(subLink.title)}
+                          onMouseLeave={handleSubLinkMouseLeave}
+                        >
+                          <div className="flex flex-col">
                             {subLink.subLinks.map((nestedLink, k) => (
-                              <button
-                                key={k}
-                                className="block px-4 py-2 text-sm font-semibold w-full text-left hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors duration-200"
-                                onClick={() => handleLinkClick(nestedLink.path)}
-                              >
-                                {nestedLink.title}
-                              </button>
+                              <Navlink key={k} item={nestedLink} className="block px-4 py-2 text-sm font-semibold" />
                             ))}
                           </div>
-                        )}
-                      </div>
-                    ))}
-                  </motion.div>
-                ) : null}
-              </AnimatePresence>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           )}
         </div>
       ))}
-    </nav>
+    </div>
   );
 };
 
