b08802e6e29d764710d6ad8b275267ab87dbfe39
Bhavish <Bhavishm009@gmail.com>
2025-01-31 20:22:23 +0530

    changes editor added

---
+++ src/app/(home)/example/page.jsx
@@ -0,0 +1,12 @@
+import Editor from '@/components/Editor'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='mt-64 w-[100%] mx-auto'>
+            <Editor/>
+        </div>
+    )
+}
+
+export default page
