35e4cf8297664c2006b1110eaa14e693debd1072
Bhavish <Bhavishm009@gmail.com>
2025-02-01 12:33:34 +0530

    chnages

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -57,6 +57,7 @@ export async function POST(req) {
             return NextResponse.json({
                 Message: "Unable to create user.",
                 Success: false,
+                Error:  err,
             });
         }
 
